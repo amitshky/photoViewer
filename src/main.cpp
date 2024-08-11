e119ab73b95e9910ab092c4981c95a20f563a5aa
amitshky <amitshkymail@gmail.com>
2024-08-11 12:06:24 +0545

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 int main() {
     InitWindow(640, 640, "Photo Viewer");
 
+    Image img = LoadImage("test/raylib_logo.jpg");
+    Texture2D texture = LoadTextureFromImage(img);
+    UnloadImage(img);
+
+    SetTargetFPS(60);
     while(!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTextureV(texture, Vector2{ 0.0f, 0.0f }, WHITE);
         DrawFPS(10, 10);
         EndDrawing();
     }
