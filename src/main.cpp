8d4238cfeae15eac9cdda8be6a386b8c23ccec84
amitshky <amitshkymail@gmail.com>
2024-08-11 12:34:35 +0545

    camera added; debug using vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include "raylib.h"
 
 int main() {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(640, 640, "Photo Viewer");
 
-    Image img = LoadImage("test/raylib_logo.jpg");
-    Texture2D texture = LoadTextureFromImage(img);
-    UnloadImage(img);
+    Texture2D img = LoadTextureFromImage(LoadImage("test/raylib_logo.jpg"));
+
+    Camera2D camera{
+        .offset = Vector2{ static_cast<float>(img.width) * 0.5f, static_cast<float>(img.height) * 0.5f },
+        .target = Vector2{ 0.0f, 0.0f },
+        .rotation = 0.0f,
+        .zoom = 1.0f,
+    };
 
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawTextureV(texture, Vector2{ 0.0f, 0.0f }, WHITE);
+        BeginMode2D(camera);
+
+        DrawTextureV(img, Vector2{ 0.0f, 0.0f }, WHITE);
         DrawFPS(10, 10);
+
+        EndMode2D();
         EndDrawing();
     }
 
