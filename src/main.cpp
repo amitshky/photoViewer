203b4202ce5c7a7bcde1c6ec4875c04a7cf0c5c6
amitshky <amitshkymail@gmail.com>
2024-08-14 18:42:23 +0545

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,38 @@
 #include <iostream>
 #include "raylib.h"
 
-void OnResize(uint64_t& width, uint64_t& height, float& aspectRatio, Camera2D& camera);
+void OnResize(Camera2D& camera, Rectangle& imgDstRec, uint64_t& width, uint64_t& height, float& winAspectRatio, const float imgAspectRatio);
 void CameraOnUpdate(Camera2D& camera);
+void CalcDstImageAspects(Rectangle& imgDstRec, const float imgAspectRatio, const float winAspectRatio, const float width, const float height);
 
 int main() {
     uint64_t width = 640;
     uint64_t height = 480;
-    float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+    float winAspectRatio = static_cast<float>(width) / static_cast<float>(height);
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(width, height, "Photo Viewer");
 
-    const Image imgData = LoadImage("test/raylib.jpg");
-    // const Image imgData = LoadImage("test/DSC02031.JPG");
+    // const Image imgData = LoadImage("test/raylib.jpg");
+    const Image imgData = LoadImage("test/DSC02031.JPG");
     Texture2D imgTex = LoadTextureFromImage(imgData);
     UnloadImage(imgData);
     const float imgAspectRatio = static_cast<float>(imgTex.width) / static_cast<float>(imgTex.height);
+    Rectangle imgSrcRec{
+        .x = 0.0f,
+        .y = 0.0f,
+        .width = static_cast<float>(imgTex.width),
+        .height = static_cast<float>(imgTex.height),
+    };
 
-    float recX = 0.0f;
-    float recY = 0.0f;
-    float recWidth = 0.0f;
-    float recHeight = 0.0f;
+    Rectangle imgDstRec{
+        .x = 0.0f,
+        .y = 0.0f,
+        .width = 0.0f,
+        .height = 0.0f,
+    };
+
+    CalcDstImageAspects(imgDstRec, imgAspectRatio, winAspectRatio, width, height);
 
     Camera2D camera{
         .offset = Vector2{ static_cast<float>(width) * 0.5f, static_cast<float>(height) * 0.5f },
@@ -35,32 +46,8 @@ int main() {
 
     while(!WindowShouldClose())
     {
-        // TODO: only change these on window resize
-        if (imgAspectRatio < aspectRatio) {
-            recX = -static_cast<float>(height) * 0.5f * imgAspectRatio;
-            recY = -static_cast<float>(height) * 0.5f;
-            recWidth = static_cast<float>(height) * imgAspectRatio;
-            recHeight = static_cast<float>(height);
-        } else {
-            recX = -static_cast<float>(width) * 0.5f;
-            recY = -static_cast<float>(width) * 0.5f * 1.0f / imgAspectRatio;
-            recWidth = static_cast<float>(width);
-            recHeight = static_cast<float>(width) * 1.0f / imgAspectRatio;
-        }
-
-        Rectangle imgSrcRec{
-            .x = 0.0f,
-            .y = 0.0f,
-            .width = static_cast<float>(imgTex.width),
-            .height = static_cast<float>(imgTex.height),
-        };
-
-        Rectangle imgDstRec{
-            .x = recX,
-            .y = recY,
-            .width = recWidth,
-            .height = recHeight,
-        };
+        OnResize(camera, imgDstRec, width, height, winAspectRatio, imgAspectRatio);
+        CameraOnUpdate(camera);
 
         BeginDrawing();
         ClearBackground(GetColor(0x282828FF));
@@ -72,25 +59,21 @@ int main() {
 
         DrawFPS(10, 10);
         EndDrawing();
-
-        OnResize(width, height, aspectRatio, camera);
-        CameraOnUpdate(camera);
     }
 
     UnloadTexture(imgTex);
     CloseWindow();
 }
 
-void OnResize(uint64_t& width, uint64_t& height, float& aspectRatio, Camera2D& camera) {
+void OnResize(Camera2D& camera, Rectangle& imgDstRec, uint64_t& width, uint64_t& height, float& winAspectRatio, const float imgAspectRatio) {
     if (!IsWindowResized())
         return;
 
     width = GetScreenWidth();
     height = GetScreenHeight();
-    aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+    winAspectRatio = static_cast<float>(width) / static_cast<float>(height);
     camera.offset = Vector2{ static_cast<float>(width) * 0.5f, static_cast<float>(height) * 0.5f };
-
-    std::cout << "widht = " << width << ", height = " << height << ", aspect ratio = " << (float)width / (float)height << '\n';
+    CalcDstImageAspects(imgDstRec, imgAspectRatio, winAspectRatio, width, height);
 }
 
 void CameraOnUpdate(Camera2D& camera) {
@@ -108,5 +91,19 @@ void CameraOnUpdate(Camera2D& camera) {
     } else if (IsKeyPressed(KEY_LEFT_BRACKET)) { // "[" to rotate counter clockwise
         camera.rotation -= 90.0f;
         camera.rotation = static_cast<float>(static_cast<int32_t>(camera.rotation) % 360);
+    }
+}
+
+void CalcDstImageAspects(Rectangle& imgDstRec, const float imgAspectRatio, const float winAspectRatio, const float width, const float height) {
+    if (imgAspectRatio < winAspectRatio) {
+        imgDstRec.x = -static_cast<float>(height) * 0.5f * imgAspectRatio;
+        imgDstRec.y = -static_cast<float>(height) * 0.5f;
+        imgDstRec.width = static_cast<float>(height) * imgAspectRatio;
+        imgDstRec.height = static_cast<float>(height);
+    } else {
+        imgDstRec.x = -static_cast<float>(width) * 0.5f;
+        imgDstRec.y = -static_cast<float>(width) * 0.5f * 1.0f / imgAspectRatio;
+        imgDstRec.width = static_cast<float>(width);
+        imgDstRec.height = static_cast<float>(width) * 1.0f / imgAspectRatio;
     }
 }
